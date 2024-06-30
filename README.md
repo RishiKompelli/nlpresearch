00e8f7a1c114a99bf8e5bce76b9b704504566fd7
Bananapower31 <rishisaikompelli@gmail.com>
2024-06-30 08:28:23 -0700

    aded dataset3 and removed unused data from readme

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # nlpresearch
 Datasets:
-https://www.kaggle.com/code/swathiunnikrishnan/mental-health-analysis-using-nlp
-https://www.kaggle.com/datasets/elvis23/mental-health-conversational-data
-https://www.kaggle.com/datasets/narendrageek/mental-health-faq-for-chatbot
 https://www.kaggle.com/datasets/thedevastator/nlp-mental-health-conversations
 https://huggingface.co/datasets/AnikaBasu/MentalHealthDataset?row=5
 https://huggingface.co/datasets/Riyazmk/mentalhealth
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ ds = load_dataset("AnikaBasu/MentalHealthDataset")
 df1 = pd.DataFrame(ds['train'])
 df1 = df1.drop('text', axis=1)
 df1 = df1.drop('input', axis = 1)
-df1 = df1[['instruction', 'output']].rename(columns={'instruction': 'Input', 'output': 'Output'})
+df1.rename(columns = {'instruction':'Input'}, inplace = True)
+df1.rename(columns = {'output':'Output'}, inplace = True)
 new_order = ['Input', 'Output']
 df1 = df1[new_order]
 
@@ -15,6 +16,13 @@ df2 = pd.read_csv("hf://datasets/Riyazmk/mentalhealth/fullMentalHealth.csv")
 df2.rename(columns = {'Questions':'Input'}, inplace = True)
 df2.rename(columns = {'Answers':'Output'}, inplace = True)
 
+#dataset 3
+df3 = pd.read_csv(r"C:\Users\rishi\OneDrive\Desktop\nlp research\nlpresearch\data\train.csv")
+df3.rename(columns = {'Context':'Input'}, inplace = True)
+df3.rename(columns = {'Response':'Output'}, inplace = True)
+
+
 df = pd.concat([df1, df2], ignore_index=True)
+df = pd.concat([df, df3], ignore_index=True)
 df = df.drop_duplicates()
 print(df)
