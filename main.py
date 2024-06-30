008172689bb83ec88847138a904d921a6b03683e
Bananapower31 <rishisaikompelli@gmail.com>
2024-06-29 22:01:56 -0700

    combined both HF datasets

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pandas as pd
 from datasets import load_dataset
 
+#dataset 1
 ds = load_dataset("AnikaBasu/MentalHealthDataset")
-df1 = pd.DataFrame(ds)
-df2 = pd.read_csv("hf://datasets/Riyazmk/mentalhealth/fullMentalHealth.csv")
-df3 = pd.read_csv(r"C:\Users\rishi\OneDrive\Desktop\nlp research\nlpresearch\data\train.csv")
+df1 = pd.DataFrame(ds['train'])
+df1 = df1.drop('text', axis=1)
+df1 = df1.drop('input', axis = 1)
+df1 = df1[['instruction', 'output']].rename(columns={'instruction': 'Input', 'output': 'Output'})
+new_order = ['Input', 'Output']
+df1 = df1[new_order]
 
-pd.merge(df1, df2, on='text', how='inner')
+#dataset 2
+df2 = pd.read_csv("hf://datasets/Riyazmk/mentalhealth/fullMentalHealth.csv")
+df2.rename(columns = {'Questions':'Input'}, inplace = True)
+df2.rename(columns = {'Answers':'Output'}, inplace = True)
+
+df = pd.concat([df1, df2], ignore_index=True)
+df = df.drop_duplicates()
+print(df)
