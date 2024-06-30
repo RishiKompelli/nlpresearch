9fda41fbc444b517e80585525c56dfb0e2875d1d
Bananapower31 <rishisaikompelli@gmail.com>
2024-06-30 09:23:00 -0700

    added datasets 3, 4, and 5

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # nlpresearch
-Datasets:
-https://www.kaggle.com/datasets/thedevastator/nlp-mental-health-conversations
-https://huggingface.co/datasets/AnikaBasu/MentalHealthDataset?row=5
-https://huggingface.co/datasets/Riyazmk/mentalhealth
+Dataset 1: https://huggingface.co/datasets/AnikaBasu/MentalHealthDataset?row=5
+Dataset 2: https://huggingface.co/datasets/Riyazmk/mentalhealth
+Dataset 3: https://www.kaggle.com/datasets/thedevastator/nlp-mental-health-conversations
+Dataset 4: https://huggingface.co/datasets/Kiran2004/MentalHealthConversations
+Dataset 5: https://huggingface.co/datasets/Mr-Bhaskar/Synthetic_Therapy_Conversations
--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import pandas as pd
 from datasets import load_dataset
 
-#dataset 1
+# dataset 1
 ds = load_dataset("AnikaBasu/MentalHealthDataset")
 df1 = pd.DataFrame(ds['train'])
-df1 = df1.drop('text', axis=1)
-df1 = df1.drop('input', axis = 1)
-df1.rename(columns = {'instruction':'Input'}, inplace = True)
-df1.rename(columns = {'output':'Output'}, inplace = True)
-new_order = ['Input', 'Output']
-df1 = df1[new_order]
+df1 = df1.drop(['text', 'input'], axis=1)
+df1.rename(columns={'instruction': 'Input', 'output': 'Output'}, inplace=True)
+df1 = df1[['Input', 'Output']]
 
-#dataset 2
+# dataset 2
 df2 = pd.read_csv("hf://datasets/Riyazmk/mentalhealth/fullMentalHealth.csv")
-df2.rename(columns = {'Questions':'Input'}, inplace = True)
-df2.rename(columns = {'Answers':'Output'}, inplace = True)
+df2.rename(columns={'Questions': 'Input', 'Answers': 'Output'}, inplace=True)
+df2 = df2.loc[:, ~df2.columns.str.contains('^Unnamed')]
 
-#dataset 3
+# dataset 3
 df3 = pd.read_csv(r"C:\Users\rishi\OneDrive\Desktop\nlp research\nlpresearch\data\train.csv")
-df3.rename(columns = {'Context':'Input'}, inplace = True)
-df3.rename(columns = {'Response':'Output'}, inplace = True)
+df3.rename(columns={'Context': 'Input', 'Response': 'Output'}, inplace=True)
+df3 = df3.loc[:, ~df3.columns.str.contains('^Unnamed')]
 
+# dataset 4
+df4 = pd.read_csv("hf://datasets/Kiran2004/MentalHealthConversations/Kiran-deppression.csv")
+df4.rename(columns={'Questions': 'Input', 'Answers': 'Output'}, inplace=True)
+df4 = df4.loc[:, ~df4.columns.str.contains('^Unnamed')]
 
-df = pd.concat([df1, df2], ignore_index=True)
-df = pd.concat([df, df3], ignore_index=True)
+# dataset 5
+splits = {'train': 'train.csv', 'validation': 'validation.csv', 'test': 'test.csv'}
+df5 = pd.read_csv("hf://datasets/Mr-Bhaskar/Synthetic_Therapy_Conversations/" + splits["train"])
+df5.rename(columns={'human': 'Input', 'ai': 'Output'}, inplace=True)
+df5 = df5.loc[:, ~df5.columns.str.contains('^Unnamed')]
+
+df = pd.concat([df1, df2, df3, df4, df5], ignore_index=True)
 df = df.drop_duplicates()
 print(df)
